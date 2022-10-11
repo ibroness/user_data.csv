d2446b66013171e9644bcb62520e17c5dcf5b043
Hussaini Ibrahim Tijjani <65361552+ibroness@users.noreply.github.com>
2022-10-11 12:01:12 +0100

    Add files via upload

---
+++ register.php
@@ -0,0 +1,11 @@
+<html>
+    <body>
+        <form action="user_data" method="post">
+            Name: <input type="text" name="fullname"><br>
+            Email: <input type="text" name="email"><br>
+            Name: <input type="radio" name="gender" value="male">Male<input type="radio" name="gender" value="female"><br>
+            country: <input type="text" name="country"><br>
+            <input type="submit">
+</form>
+</body>
+</html>
---
+++ user_data.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+    Name: <?php 
+    print $_POST["name"]; ?><br>
+
+    Email:<?php  print $_POST["email"]; ?><br>
+
+    Date of Birth: <?php print $_POST["dateofbirth"]; ?><br>
+
+   Gender: <?php print $_POST["gender"]; ?><br>
+
+   Country: <?php print $_POST["country"]; ?>
+    </body>
+    </html>
+    
